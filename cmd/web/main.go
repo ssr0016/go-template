6f602c61740d350f0880d8c54caf1e1915ed090d
ssr0016 <samsonrecluta0016@gmail.com>
2025-07-12 11:27:08 +0800

    Installing and setting up a session package

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,7 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/alexedwards/scs/v2"
 	"github.com/ssr0016/booking/pkg/config"
 	"github.com/ssr0016/booking/pkg/handlers"
 	"github.com/ssr0016/booking/pkg/render"
@@ -12,9 +14,21 @@ import (
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main is the main application function
 func main() {
-	var app config.AppConfig
+	// change this to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module github.com/ssr0016/booking
 
 go 1.22.5
 
-require github.com/go-chi/chi/v5 v5.2.2
-
-require github.com/justinas/nosurf v1.2.0 // indirect
+require (
+	github.com/alexedwards/scs/v2 v2.5.0
+	github.com/go-chi/chi/v5 v5.2.2
+	github.com/justinas/nosurf v1.2.0
+)

5378cef25abca9e689466ceff55a524430fcdb0f
ssr0016 <samsonrecluta0016@gmail.com>
2025-07-23 19:46:17 +0800

    adding migration for seeding rooms

---
+++ migrations/20250723113856_create_seed_rooms_tables.postgres.up.sql
@@ -0,0 +1,16 @@
+INSERT INTO
+    public.rooms (
+        room_name,
+        created_at,
+        updated_at
+    )
+VALUES (
+        'Genaral''s Quarters',
+        '2020-11-18 00:00:00.000',
+        '2020-11-18 00:00:00.000'
+    ),
+    (
+        'Major''s Suite',
+        '2020-11-18 00:00:00.000',
+        '2020-11-18 00:00:00.000'
+    );

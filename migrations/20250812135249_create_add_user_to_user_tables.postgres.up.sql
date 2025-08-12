713cca02e929786db531b976dd0570833271945f
ssr0016 <samsonrecluta0016@gmail.com>
2025-08-12 22:12:59 +0800

    add admin user migrations

---
+++ migrations/20250812135249_create_add_user_to_user_tables.postgres.up.sql
@@ -0,0 +1,4 @@
+INSERT INTO "public"."users"("first_name","last_name","email","password","access_level","created_at","updated_at")
+VALUES
+(E'ssr0016',E'recs',E'admin@gmail.com',E'$2a$12$5mwVessNIvkjTY3VLGaGk.tG7g5aH5uMyTezgJjb6Vr0rlTsCJJNG',E'3',E'2025-07-29 00:00:00',E'2025-07-29 00:00:00');
+
